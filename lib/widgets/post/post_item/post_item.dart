5c9428f0711fa994395d579e12e7b2ae8cbd4180
handsomelong922 <handsomelong922@users.noreply.github.com>
2026-07-04 03:54:28 +0800

    restore short-post selection context

--- lib/widgets/post/post_item/post_item.dart
+++ lib/widgets/post/post_item/post_item.dart
@@ -195,7 +195,6 @@ class _PostItemState extends ConsumerState<PostItem> {
                   searchHighlightQuery: widget.searchHighlightQuery,
                   post: post,
                   topicId: widget.topicId,
-                  enableSelectionArea: false,
                   onQuoteImage: widget.onQuoteImage,
                   onInternalLinkTap:
                       (topicId, topicSlug, postNumber, {initialNestedView}) {
